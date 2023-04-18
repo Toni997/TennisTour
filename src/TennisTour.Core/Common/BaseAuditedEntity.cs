32b4a5694e619951febf219b657fd28063ca1563
tonikazinoti <tk48322@unist.hr>
2023-04-18 07:39:05 +0200

    base audited entity

---
+++ src/TennisTour.Core/Common/BaseAuditedEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisTour.Core.Common
+{
+    public abstract class BaseAuditedEntity : IAuditedEntity
+    {
+        public Guid Id { get; set; }
+        public abstract string CreatedBy { get; set; }
+        public abstract DateTime CreatedOn { get; set; }
+        public abstract string UpdatedBy { get; set; }
+        public abstract DateTime? UpdatedOn { get; set; }
+    }
+}
